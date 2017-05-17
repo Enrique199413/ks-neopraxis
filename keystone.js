a53f700b951c77b4a939ac926dfa96edda68ec57
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-17 16:21:53 -0500

    #2 add the model of Post

--- keystone.js
+++ keystone.js
@@ -49,6 +49,7 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
+	posts: ['posts', 'post-categories'],
 	enquiries: 'enquiries',
 	users: 'users',
 });
