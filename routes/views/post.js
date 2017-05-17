75de043dc8eeee0ab9a1fbc6ef256313f75134ca
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-17 18:00:18 -0500

    #2 change the syntax

--- routes/views/post.js
+++ routes/views/post.js
@@ -35,15 +35,26 @@ exports = module.exports = function (req, res) {
 	// Load other posts
 	view.on('init', function (next) {
 
-		var q = keystone.list('Post').model.find().where('state', 'published').sort('-publishedDate').populate('author').limit('4');
+		var q = keystone.list('Post').paginate({
+			page: req.query.page || 1,
+			perPage: 10,
+			maxPages: 10,
+			filters: {
+				state: 'published',
+			},
+		})
+			.sort('-publishedDate')
+			.populate('author categories');
+
+		if (locals.data.category) {
+			q.where('categories').in([locals.data.category]);
+		}
 
 		q.exec(function (err, results) {
-			locals.data.posts = results;
+			locals.data.recents = results;
 			next(err);
 		});
-
 	});
-
 	// Render the view
 	view.render('post');
 };
