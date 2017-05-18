b527fa539351c8c44a249770df4cda4415ae846d
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-18 11:43:41 -0500

    #2 add the blog neswletter

--- public/js/blick.utils.js
+++ public/js/blick.utils.js
@@ -4,8 +4,38 @@
 /* global $ */
 
 $('document').ready(function () {
+	$('#blogBody > p:nth-child(3)').addClass('neopraxis-font-myriadpro-regular no-margin-text charcoal-text blog-text')
+	$('#blogBody > a').addClass('neopraxis-font-myriadpro-regular no-margin-text charcoal-text blog-text')
 	$('#messageNewsletter').hide();
-	$('#newsletterInput').on('submit',function (e) {
+	$('#messageNewsletterBlog').hide();
+	$('#blogNewsletter').on('submit', function (e) {
+		$('#inputNewsletterBlog').hide();
+		$.ajax({
+			type: 'post',
+			url: '/api/mailchimp/newsletter',
+			data: $('#blogNewsletter').serialize(),
+			success: function () {
+				document.querySelector('#messageNewsletterBlog').innerHTML = 'Gracias por tu suscripción';
+				setTimeout(function () {
+					document.querySelector('#blogNewsletter').reset();
+					$('#inputNewsletterBlog').show();
+					$('#messageNewsletter').hide();
+				}, 2000);
+			},
+			error: function (error) {
+				document.querySelector('#messageNewsletterBlog').innerHTML = error.status === 403 ? 'Ya estas registrado' : 'Verifica el correo';
+				document.querySelector('#messageNewsletterBlog').style.display = 'inline !important';
+				$('#messageNewsletterBlog').show();
+				setTimeout(function () {
+					$('#inputNewsletterBlog').show();
+					$('#messageNewsletterBlog').hide();
+					document.querySelector('#blogNewsletter').reset();
+				}, 2000);
+			},
+		});
+		e.preventDefault();
+	});
+	$('#newsletterInput').on('submit', function (e) {
 		$('#inputText').hide();
 		$.ajax({
 			type: 'post',
