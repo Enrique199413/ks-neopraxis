1427a37bf736a6fcc7c4a5ee4d048304b7f7c2ed
MitchelHdz <sebastianvicky_@hotmail.com>
2017-05-25 12:30:05 -0500

    #9 Fixing some scripts

--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		else{
 			var hash = this.hash;
 			$('html, body').animate({
-				scrollTop: $(hash).offset().top -90
+				scrollTop: $(hash).offset().top -83
 			}, 800, function () {
 				window.location.hash = hash;
 
